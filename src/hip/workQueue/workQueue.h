51468c0518dae570ca3d0a1734e30bef51f6bcdf
Piotr Kowalczyk <piotr@a2labs.com>
2025-10-07 13:11:10 +0000

    [scheduler]: WorkQueue WIP

---
+++ src/hip/workQueue/workQueue.h
@@ -0,0 +1,72 @@
+#pragma once
+
+// This queue is unusual, since the assumption is that
+// all items slots are preallocated and there is enough of them, so there is no
+// need to reuse item memory again.
+template <typename T, unsigned int SIZE>
+struct WorkQueue {
+
+  struct __align__(16) QueueSlot {
+    int isComitted;
+    T* data;
+  };
+
+  QueueSlot* m_workQueue;
+  unsigned int* m_headIdx;
+  unsigned int* m_tailIdx;
+
+  __host__ void Init() {
+    HIP_CHECK(hipMalloc(&m_workQueue, SIZE * sizeof(QueueSlot)));
+    HIP_CHECK(hipMalloc(&m_headIdx, sizeof(int*)));
+    HIP_CHECK(hipMalloc(&m_tailIdx, sizeof(int*)));
+
+    HIP_CHECK(hipMemset(m_workQueue, 0, SIZE * sizeof(QueueSlot)));
+
+    const int zero = 0;
+
+    HIP_CHECK(hipMemcpy(m_headIdx, &zero, sizeof(int), hipMemcpyHostToDevice));
+    HIP_CHECK(hipMemcpy(m_tailIdx, &zero, sizeof(int), hipMemcpyHostToDevice));
+  }
+
+  __host__ void Deinit() {
+    HIP_CHECK(hipFree(m_tailIdx));
+    HIP_CHECK(hipFree(m_headIdx));
+    HIP_CHECK(hipFree(m_workQueue));
+  }
+
+  __device__ bool Push(const T* item_global_ptr) {
+    unsigned int slotIdx = atomicAdd(m_headIdx, 1);
+
+    if (slotIdx >= SIZE) {
+      assert(false);
+      return false;
+    }
+
+    WorkSlot* slot = &m_workQueue[slotIdx];
+    slot->data = item_global_ptr;
+
+    __threadfence();
+    const int prev = atomicExch(&slot->isComitted, 1);
+    assert(prev == 0);
+
+    return true;
+  }
+
+  __device__ T* PopOrBusyWait() {
+    unsigned int slotIdx = atomicAdd(m_tailIdx, 1);
+    unsigned int currentHeadIdx = __atomic_load_n(m_headIdx, __ATOMIC_RELAXED);
+
+    while (currentHeadIdx <= slotIdx) {
+      // Wait until there is something to pop.
+      currentHeadIdx = __atomic_load_n(m_headIdx, __ATOMIC_RELAXED);
+    }
+
+    while (__atomic_load_n(&slot->isComitted, __ATOMIC_RELAXED) == 0) {
+      // Wait until item is commited.
+    }
+
+    __threadfence(); //< Load with acquire semantics should be enough?
+    QueueSlot* slot = &m_workQueue[slotIdx];
+    return slot->data;
+  }
+};
