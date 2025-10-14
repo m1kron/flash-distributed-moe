9e743b8f9c4f36b93f64c13d57e3d6f890b20086
Piotr Kowalczyk <piotr@a2labs.com>
2025-10-14 14:06:39 +0000

    [taskManager]: Refactoring, code clean up.

--- src/hip/taskSystem/taskManager.h
+++ src/hip/taskSystem/taskManager.h
@@ -1,7 +1,7 @@
 #pragma once
+#include "../utils/hipDeviceUtils.h"
 #include "taskAllocator.h"
 #include "workQueue.h"
-#include "../utils/hipDeviceUtils.h"
 
 // Note: For now I will try approach with single global work queue for all
 // workers. This might be probelmatic due to contention, but it is implemented
@@ -13,131 +13,195 @@ template <typename TTask, unsigned int SIZE>
 struct TaskManager {
   TaskAllocator<TTask, SIZE> _tasksAlloc;
   WorkQueue<TTask, SIZE> _workQueue;
-  uint32_t* _maxTasks;
+  uint32_t* _expectedMaxTasks;
   uint32_t* _currentExecutedTasks;
 
-  __host__ hipError_t Init(unsigned expectedMaxTasks) {
-    HIP_ERROR_CHECK(_workQueue.Init());
-    HIP_ERROR_CHECK(_tasksAlloc.Init());
-    HIP_ERROR_CHECK(hipMalloc(&_maxTasks, sizeof(uint32_t)));
-    HIP_ERROR_CHECK(hipMemcpy(_maxTasks, &expectedMaxTasks, sizeof(uint32_t),
-                        hipMemcpyHostToDevice));
-    HIP_ERROR_CHECK(hipMalloc(&_currentExecutedTasks, sizeof(uint32_t)));
-    HIP_ERROR_CHECK(hipMemset(_currentExecutedTasks, 0, sizeof(uint32_t)));
-    return hipSuccess;
-  }
+  // Initializes task manager on host.
+  __host__ hipError_t Init(unsigned expectedMaxTasks);
 
-  __host__ hipError_t Deinit() {
-    HIP_ERROR_CHECK(_workQueue.Deinit());
-    HIP_ERROR_CHECK(_tasksAlloc.Deinit());
-    HIP_ERROR_CHECK(hipFree(_maxTasks));
-    HIP_ERROR_CHECK(hipFree(_currentExecutedTasks));
-    return hipSuccess;
-  }
+  // Deinitializes task manager on host.
+  __host__ hipError_t Deinit();
 
-  __device__ uint32_t GetCurrentExecutedTasks() const {
-    return __atomic_load_n(_currentExecutedTasks, __ATOMIC_RELAXED);
-  }
+  // Gets the current number of executed tasks.
+  __device__ uint32_t GetCurrentExecutedTasks() const;
 
-  __device__ void IncrementCurrentExecutedTasks() {
-    atomicAdd(_currentExecutedTasks, 1);
-  }
+  // Increments the current number of executed tasks.
+  __device__ void IncrementCurrentExecutedTasks();
 
-  __device__ bool IsAtMaxExecuted() const {
-    // read atomically and compare with max
-    const uint32_t maxExecutedTasks =
-        __atomic_load_n(_maxTasks, __ATOMIC_RELAXED);
-    return GetCurrentExecutedTasks() >= maxExecutedTasks;
-  }
+  // Checks if the current number of executed tasks reached the max.
+  __device__ bool DidExecuteExpectedNumberOfTasks() const;
 
-  __device__ TTask* AllocateTask() { return _tasksAlloc.Allocate(); }
+  // Allocates new task in device code, returns nullptr if out of memory.
+  __device__ TTask* AllocateTask();
 
-  __device__ void FreeTask(TTask* task) { _tasksAlloc.Free(task); }
+  // Frees task in device code.
+  __device__ void FreeTask(TTask* task);
 
-  __device__ uint32_t GetSize() const { return _tasksAlloc.GetSize(); }
+  // Pushes task to the queue in device code, executed by warp 0 of the block.
+  __device__ void PushTask_warp(TTask* task);
 
-  __device__ void PushTask_warp(TTask* task) {
-    if (threadIdx.x / warpSize == 0) {
-      TTask* task_globalMem = nullptr;
+  // Waits and pops new task from the queue in device code, executed by warp 0
+  // of the block.
+  // If there is a new task ready, warp loads it into task_sharedMem.
+  // If reached excpected max of executed tasks, waiting loop is break and code
+  // returns true. Otherwise return false, and then task_sharedMem contains
+  // poped task.
+  __device__ bool WaitAndPopTask_warp(TTask* task_sharedMem);
+};
 
-      if (threadIdx.x == 0) {
-        // Thread 0 allocates task in global mem:
-        task_globalMem = AllocateTask();
-      }
+/////////////////////////////////////////////////////////////////
+//
+// IMPLEMENTATION:
+//
+/////////////////////////////////////////////////////////////////
 
-      // Warp 0 cooperatvely fill task in global mem:
-      unsigned long long allocatedTaskInt =
-          reinterpret_cast<unsigned long long>(task_globalMem);
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __host__ hipError_t
+TaskManager<TTask, SIZE>::Init(unsigned expectedMaxTasks) {
+  HIP_ERROR_CHECK(_workQueue.Init());
+  HIP_ERROR_CHECK(_tasksAlloc.Init());
+  HIP_ERROR_CHECK(hipMalloc(&_expectedMaxTasks, sizeof(uint32_t)));
+  HIP_ERROR_CHECK(hipMemcpy(_expectedMaxTasks, &expectedMaxTasks,
+                            sizeof(uint32_t), hipMemcpyHostToDevice));
+  HIP_ERROR_CHECK(hipMalloc(&_currentExecutedTasks, sizeof(uint32_t)));
+  HIP_ERROR_CHECK(hipMemset(_currentExecutedTasks, 0, sizeof(uint32_t)));
+  return hipSuccess;
+}
 
-      // Broadcast pointer to all threads in the warp.
-      allocatedTaskInt = __shfl(allocatedTaskInt, 0);
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __host__ hipError_t TaskManager<TTask, SIZE>::Deinit() {
+  HIP_ERROR_CHECK(_workQueue.Deinit());
+  HIP_ERROR_CHECK(_tasksAlloc.Deinit());
+  HIP_ERROR_CHECK(hipFree(_expectedMaxTasks));
+  HIP_ERROR_CHECK(hipFree(_currentExecutedTasks));
+  return hipSuccess;
+}
 
-      task_globalMem = reinterpret_cast<TTask*>(allocatedTaskInt);
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __device__ uint32_t
+TaskManager<TTask, SIZE>::GetCurrentExecutedTasks() const {
+  return __atomic_load_n(_currentExecutedTasks, __ATOMIC_RELAXED);
+}
 
-      char* taskBlob_g = reinterpret_cast<char*>(task_globalMem);
-      const char* taskBlob_r = reinterpret_cast<const char*>(task);
-      for (int i = threadIdx.x; i < sizeof(TTask); i += warpSize) {
-        taskBlob_g[i] = taskBlob_r[i];
-      }
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __device__ void
+TaskManager<TTask, SIZE>::IncrementCurrentExecutedTasks() {
+  atomicAdd(_currentExecutedTasks, 1);
+}
 
-      __threadfence();  //< Mem for writing warp only
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __device__ bool
+TaskManager<TTask, SIZE>::DidExecuteExpectedNumberOfTasks() const {
+  // read atomically and compare with max
+  const uint32_t expectedMaxExecutedTasks =
+      __atomic_load_n(_expectedMaxTasks, __ATOMIC_RELAXED);
+  return GetCurrentExecutedTasks() >= expectedMaxExecutedTasks;
+}
 
-      // Task is ready in global mem.
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __device__ TTask* TaskManager<TTask, SIZE>::AllocateTask() {
+  return _tasksAlloc.Allocate();
+}
 
-      // Thread 0 pushes task to the queue:
-      if (threadIdx.x == 0) {
-        _workQueue.Push(task_globalMem);
-        HIP_DEVICE_LOG("Added task to the queue\n");
-      }
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __device__ void TaskManager<TTask, SIZE>::FreeTask(TTask* task) {
+  _tasksAlloc.Free(task);
+}
+
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __device__ void TaskManager<TTask, SIZE>::PushTask_warp(TTask* task) {
+  if (threadIdx.x / warpSize == 0) {
+    TTask* task_globalMem = nullptr;
+
+    if (threadIdx.x == 0) {
+      // Thread 0 allocates task in global mem:
+      task_globalMem = AllocateTask();
+      HIP_DEVICE_ASSERT(task_globalMem != nullptr);
+    }
+
+    // Warp 0 cooperatvely fill task in global mem:
+    unsigned long long allocatedTaskInt =
+        reinterpret_cast<unsigned long long>(task_globalMem);
+
+    // Broadcast pointer to all threads in the warp.
+    allocatedTaskInt = __shfl(allocatedTaskInt, 0);
+
+    task_globalMem = reinterpret_cast<TTask*>(allocatedTaskInt);
+
+    char* taskBlob_g = reinterpret_cast<char*>(task_globalMem);
+    const char* taskBlob_r = reinterpret_cast<const char*>(task);
+    for (int i = threadIdx.x; i < sizeof(TTask); i += warpSize) {
+      taskBlob_g[i] = taskBlob_r[i];
+    }
+
+    __threadfence();  //< Mem for writing warp only
+
+    // Task is ready in global mem.
+
+    // Thread 0 pushes task to the queue:
+    if (threadIdx.x == 0) {
+      _workQueue.Push(task_globalMem);
+      HIP_DEVICE_LOG("Added task to the queue\n");
     }
   }
+}
 
-  __device__ bool PopTask_warp(TTask* task_sharedMem) {
-    // Get new task:
-    if (threadIdx.x / warpSize == 0) {
-      TTask* task_globalMem = nullptr;
+/////////////////////////////////////////////////////////////////
+template <typename TTask, unsigned int SIZE>
+inline __device__ bool TaskManager<TTask, SIZE>::WaitAndPopTask_warp(
+    TTask* task_sharedMem) {
+  // Get new task:
+  if (threadIdx.x / warpSize == 0) {
+    TTask* task_globalMem = nullptr;
 
-      bool shouldInterrupt = false;
-      if (threadIdx.x == 0) {
-        const auto ticket = _workQueue.ReserveSlotTicket();
-        while (!_workQueue.TryToPop(ticket, task_globalMem)) {
-          if (IsAtMaxExecuted()) {
-            // We are at max executed tasks, return false - no task for now.
-            shouldInterrupt = true;
-            HIP_DEVICE_LOG("Breaking waiting loop, since max tasks reached\n");
-            break;
-          }
+    bool shouldInterrupt = false;
+    if (threadIdx.x == 0) {
+      const auto ticket = _workQueue.ReserveSlotTicket();
+      while (!_workQueue.TryToPop(ticket, task_globalMem)) {
+        if (DidExecuteExpectedNumberOfTasks()) {
+          shouldInterrupt = true;
+          HIP_DEVICE_LOG(
+              "Breaking waiting loop, since max expected tasks reached\n");
+          break;
         }
       }
+    }
 
-      shouldInterrupt = __shfl(shouldInterrupt, 0);
-      if (shouldInterrupt) {
-        return true;
-      }
+    shouldInterrupt = __shfl(shouldInterrupt, 0);
+    if (shouldInterrupt) {
+      return true;
+    }
 
-      unsigned long long poppedTaskInt =
-          reinterpret_cast<unsigned long long>(task_globalMem);
+    unsigned long long poppedTaskInt =
+        reinterpret_cast<unsigned long long>(task_globalMem);
 
-      // Broadcast pointer to all threads in the warp.
-      poppedTaskInt = __shfl(poppedTaskInt, 0);
+    // Broadcast pointer to all threads in the warp.
+    poppedTaskInt = __shfl(poppedTaskInt, 0);
 
-      task_globalMem = reinterpret_cast<TTask*>(poppedTaskInt);
+    task_globalMem = reinterpret_cast<TTask*>(poppedTaskInt);
 
-      char* taskBlob_s = reinterpret_cast<char*>(task_sharedMem);
-      const char* taskBlob_g = reinterpret_cast<char*>(task_globalMem);
+    char* taskBlob_s = reinterpret_cast<char*>(task_sharedMem);
+    const char* taskBlob_g = reinterpret_cast<char*>(task_globalMem);
 
-      // Warp 0 cooperatvely copy task from global mem to shared mem:
-      for (int i = threadIdx.x; i < sizeof(TTask); i += warpSize) {
-        taskBlob_s[i] = taskBlob_g[i];
-      }
+    // Warp 0 cooperatvely copy task from global mem to shared mem:
+    for (int i = threadIdx.x; i < sizeof(TTask); i += warpSize) {
+      taskBlob_s[i] = taskBlob_g[i];
+    }
 
-      if (threadIdx.x == 0) {
-        // Free task in global mem:
-        FreeTask(task_globalMem);
-        IncrementCurrentExecutedTasks();
-      }
-      return false;
+    if (threadIdx.x == 0) {
+      // Free task in global mem:
+      FreeTask(task_globalMem);
+      IncrementCurrentExecutedTasks();
     }
     return false;
   }
-};
+  return false;
+}
