bda81d0fbd61a6145699ffdae68ba944522ecc10
Piotr Kowalczyk <piotr@a2labs.com>
2025-10-14 13:42:25 +0000

    [taskAllocator]: Refactoring, code clean up.

--- src/hip/taskSystem/taskAllocator.h
+++ src/hip/taskSystem/taskAllocator.h
@@ -2,31 +2,23 @@
 #include "../hipHostCommon.h"
 #include "../utils/hipDeviceUtils.h"
 
+// Simple and fast linear allocator for tasks.
 template <typename T, unsigned int SIZE>
 struct TaskAllocator {
-  T* _items;
-  uint32_t* _allocatedIdx;
-
+  // Initializes allocator on host, allocates memory for SIZE items of type T.
   __host__ hipError_t Init();
 
-  __host__ hipError_t Deinit() {
-    HIP_ERROR_CHECK(hipFree(_items));
-    HIP_ERROR_CHECK(hipFree(_allocatedIdx));
-    return hipSuccess;
-  }
+  // Deinitializes allocator on host, frees allocated memory.
+  __host__ hipError_t Deinit();
 
-  __device__ T* Allocate() {
-    const uint32_t idx = atomicAdd(_allocatedIdx, 1);
-    if (idx >= SIZE) {
-      HIP_DEVICE_LOG("TaskAllocator: Out of memory!\n");
-      return nullptr;
-    }
-    return _items + idx;
-  }
+  // Allocates new task in device code, returns nullptr if out of memory.
+  __device__ T* Allocate();
 
-  __device__ void Free(T* item);
+  // Frees task in device code.
+  __device__ void Free(T* task);
 
-  __device__ uint32_t GetSize() const { return SIZE; }
+  T* m_buffer;
+  uint32_t* m_allocatedIdx;
 };
 
 /////////////////////////////////////////////////////////////////
@@ -35,17 +27,37 @@ struct TaskAllocator {
 //
 /////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////
 template <typename T, unsigned int SIZE>
-inline __device__ void TaskAllocator<T, SIZE>::Free(T* item) {
-  // Nothing to do, allocator does not reuse memory.
+inline __host__ hipError_t TaskAllocator<T, SIZE>::Init() {
+  HIP_ERROR_CHECK(hipMalloc(&m_allocatedIdx, sizeof(uint32_t)));
+  HIP_ERROR_CHECK(hipMemset(m_allocatedIdx, 0, sizeof(uint32_t)));
+  HIP_ERROR_CHECK(hipMalloc(&m_buffer, SIZE * sizeof(T)));
+  HIP_ERROR_CHECK(hipMemset(m_buffer, 0, SIZE * sizeof(T)));
+  return hipSuccess;
 }
 
 /////////////////////////////////////////////////////////////////
 template <typename T, unsigned int SIZE>
-inline __host__ hipError_t TaskAllocator<T, SIZE>::Init() {
-  HIP_ERROR_CHECK(hipMalloc(&_allocatedIdx, sizeof(uint32_t)));
-  HIP_ERROR_CHECK(hipMemset(_allocatedIdx, 0, sizeof(uint32_t)));
-  HIP_ERROR_CHECK(hipMalloc(&_items, SIZE * sizeof(T)));
-  HIP_ERROR_CHECK(hipMemset(_items, 0, SIZE * sizeof(T)));
+inline __host__ hipError_t TaskAllocator<T, SIZE>::Deinit() {
+  HIP_ERROR_CHECK(hipFree(m_buffer));
+  HIP_ERROR_CHECK(hipFree(m_allocatedIdx));
   return hipSuccess;
+}
+
+/////////////////////////////////////////////////////////////////
+template <typename T, unsigned int SIZE>
+inline __device__ T* TaskAllocator<T, SIZE>::Allocate() {
+  const uint32_t idx = atomicAdd(m_allocatedIdx, 1);
+  if (idx >= SIZE) {
+    HIP_DEVICE_LOG("TaskAllocator: Out of memory!\n");
+    return nullptr;
+  }
+  return m_buffer + idx;
+}
+
+/////////////////////////////////////////////////////////////////
+template <typename T, unsigned int SIZE>
+inline __device__ void TaskAllocator<T, SIZE>::Free(T* task) {
+  // Nothing to do, allocator does not reuse memory.
 }
