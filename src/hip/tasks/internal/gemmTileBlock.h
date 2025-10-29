2c661eeda5f1b23834cad47480ebea5906410c0a
Piotr Kowalczyk <piotr@a2labs.com>
2025-10-29 13:20:14 +0000

    [moe]: All task internal functions now take number of block thread as a parameter.

--- src/hip/tasks/internal/gemmTileBlock.h
+++ src/hip/tasks/internal/gemmTileBlock.h
@@ -1,6 +1,6 @@
 #pragma once
 
-template <int _N, int _K, int _TILE_M, int _TILE_N, int _TILE_K>
+template <int _N, int _K, int _TILE_M, int _TILE_N, int _TILE_K, int _THREADS>
 struct GemmTileParams {
   using TOutputType = float;
   using TInputType = float;
@@ -9,7 +9,7 @@ struct GemmTileParams {
   constexpr static int TILE_M = _TILE_M;
   constexpr static int TILE_N = _TILE_N;
   constexpr static int TILE_K = _TILE_K;
-  constexpr static int THREADS = 256;
+  constexpr static int THREADS = _THREADS;
   constexpr static int THREAD_OUTPUT_SIZE = (TILE_M * TILE_N) / THREADS;
   constexpr static int SHARED_MEM_NEEDES_BYTES =
       ((TILE_M * TILE_K) + (TILE_K * TILE_N)) * sizeof(TInputType);
--- test/cpp/taskSystemUnittests/gemmTest/tileGemm.h
+++ test/cpp/taskSystemUnittests/gemmTest/tileGemm.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "../../../../src/hip/tasks/internal/gemmTileBlock.h"
 
+constexpr int TILE_M = 64;
+constexpr int TILE_N = 32;
+constexpr int THREADS = 128;
+
 template <int N>
 __device__ void gemm_64x64x2048_single_block(const float* __restrict__ A,
                                              const float* __restrict__ B,
                                              float* __restrict__ C,
                                              int blockTileRowStartIdx,
                                              int blockTileColStartIdx) {
-  using TGemmTileParams = GemmTileParams<N, 2048, 64, 64, 32>;
+  using TGemmTileParams = GemmTileParams<N, 2048, TILE_M, TILE_N, 32, THREADS>;
 
   typename TGemmTileParams::TOutputType
       out_regs[TGemmTileParams::THREAD_OUTPUT_SIZE];
