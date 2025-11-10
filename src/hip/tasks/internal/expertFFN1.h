a65b22e2250645c689a401fc2a04c10ca9765904
Piotr Kowalczyk <piotr@a2labs.com>
2025-11-10 14:38:13 +0000

    [moeExpert]: FFN1 task: Refactoring, changed template parameter to represent  output tiling.

--- src/hip/tasks/internal/expertFFN1.h
+++ src/hip/tasks/internal/expertFFN1.h
@@ -17,34 +17,38 @@ inline __device__ float silu(float a, float b) {
 }
 
 // Task for expert's FFN1. rowIdx and colIdx refers to output tiles.
-template <typename TGEMM_TILE>
+// Assumption is that output's N dimension equal to 2 * (expertWeights N
+// dimension).
+template <typename T_OUTPUT_TILE>
 __device__ void expertFFN1Task(const float* __restrict__ tokens,
                                const float* __restrict__ expertWeights,
                                float* __restrict__ output, int rowIdx,
                                int colIdx, void* sharedMemPool) {
-  constexpr int N_CHUNKS = (TGEMM_TILE::N / 2) / TGEMM_TILE::TILE_N;
+  constexpr int N_CHUNKS = T_OUTPUT_TILE::N / T_OUTPUT_TILE::TILE_N;
 
   const int tokenIdx = rowIdx;
   const int tileCol = colIdx;
 
-  // TODO: Fuse gemms.
-  typename TGEMM_TILE::TOutputType w1_regs[TGEMM_TILE::THREAD_OUTPUT_SIZE];
-  GemmTile_block<TGEMM_TILE>(tokens, expertWeights, w1_regs, tokenIdx, tileCol,
-                             sharedMemPool);
+  using T_FFN1_TILE =
+      GemmTileParams<T_OUTPUT_TILE::N * 2, T_OUTPUT_TILE::K,
+                     T_OUTPUT_TILE::TILE_M, T_OUTPUT_TILE::TILE_N,
+                     T_OUTPUT_TILE::TILE_K, T_OUTPUT_TILE::THREADS>;
 
-  typename TGEMM_TILE::TOutputType w3_regs[TGEMM_TILE::THREAD_OUTPUT_SIZE];
-  GemmTile_block<TGEMM_TILE>(tokens, expertWeights, w3_regs, tokenIdx,
-                             tileCol + N_CHUNKS, sharedMemPool);
+  // TODO: Fuse gemms.
+  typename T_FFN1_TILE::TOutputType w1_regs[T_FFN1_TILE::THREAD_OUTPUT_SIZE];
+  GemmTile_block<T_FFN1_TILE>(tokens, expertWeights, w1_regs, tokenIdx, tileCol,
+                              sharedMemPool);
+
+  typename T_FFN1_TILE::TOutputType w3_regs[T_FFN1_TILE::THREAD_OUTPUT_SIZE];
+  GemmTile_block<T_FFN1_TILE>(tokens, expertWeights, w3_regs, tokenIdx,
+                              tileCol + N_CHUNKS, sharedMemPool);
 
   // Silu:
-  for (int i = 0; i < TGEMM_TILE::THREAD_OUTPUT_SIZE; ++i) {
+  for (int i = 0; i < T_FFN1_TILE::THREAD_OUTPUT_SIZE; ++i) {
     w3_regs[i] = silu(w1_regs[i], w3_regs[i]);
   }
 
   // w3_regs contains output
-  using T_OUTPUT_TILE = GemmTileParams<TGEMM_TILE::N / 2, TGEMM_TILE::K,
-                                       TGEMM_TILE::TILE_M, TGEMM_TILE::TILE_N,
-                                       TGEMM_TILE::TILE_K, TGEMM_TILE::THREADS>;
 
   WriteGemmTileToGlobalMem_block<T_OUTPUT_TILE>(w3_regs, output, tokenIdx,
                                                 tileCol);
