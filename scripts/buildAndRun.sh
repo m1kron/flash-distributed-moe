2dc2862d68d8d5e9d3d26b2f2ff45247bc7706ba
Piotr Kowalczyk <piotr@a2labs.com>
2025-11-20 11:50:12 +0000

    [moe][scripts]: Reverted temp changes in buildAndRun.sh

--- scripts/buildAndRun.sh
+++ scripts/buildAndRun.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 
 ./scripts/cmake_build.sh
 
-HIP_VISIBLE_DEVICES=1 ../build/test/cpp/unittests_app --gtest_filter="*MoeFull*"
+HIP_VISIBLE_DEVICES=1 ../build/test/cpp/unittests_app
