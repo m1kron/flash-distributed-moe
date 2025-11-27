7a043e072f3ecede939ab6ec4b8e9395b5d1af9b
Piotr Kowalczyk <piotr@a2labs.com>
2025-11-27 13:59:48 +0000

    [moe][gemms]: Now gemm assume transposed B - same as vllm.

--- test/cpp/moe/reference/refGemm.h
+++ test/cpp/moe/reference/refGemm.h
@@ -1,29 +1,28 @@
 #pragma once
 
-#include <cstddef>
-#include <cstring>
 #include <vector>
 
 namespace test {
 
+// A: M x K (row-major)
+// B: N x K (row-major)  [note: transposed versus the usual K x N]
+// C: M x N (row-major)
+// Computes C = A * B^T
 template <typename T>
 inline std::vector<T> refGemm(const T* A, const T* B, int M, int N, int K) {
-  // Row-major:
-  //  A: M x K
-  //  B: K x N
-  //  C: M x N
-
-  std::vector<T> out(M * N, T(0));
+  std::vector<T> out(size_t(M) * size_t(N), T(0));
   T* C = out.data();
 
   for (int i = 0; i < M; ++i) {
+    const T* Arow = A + size_t(i) * size_t(K);
     T* Crow = C + size_t(i) * size_t(N);
-    for (int k = 0; k < K; ++k) {
-      const T a = A[size_t(i) * size_t(K) + size_t(k)];
-      const T* Brow = B + size_t(k) * size_t(N);
-      for (int j = 0; j < N; ++j) {
-        Crow[j] += a * Brow[j];
+    for (int j = 0; j < N; ++j) {
+      const T* Brow = B + size_t(j) * size_t(K);  // B row j (length K)
+      T acc = T(0);
+      for (int k = 0; k < K; ++k) {
+        acc += Arow[k] * Brow[k];
       }
+      Crow[j] = acc;
     }
   }
 
