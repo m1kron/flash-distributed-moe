11c57da6bdee5c47d4cdddc2d7193cc8037b8af7
Piotr Kowalczyk <piotr@a2labs.com>
2025-12-01 17:03:29 +0000

    [moe][cpp]: Added moe kernel benchmark.

---
+++ test/cpp/benchmarks/moeKernelBenchmark.cpp
@@ -0,0 +1,90 @@
+#include <vector>
+
+#include "gtest/gtest.h"
+#include "include/iMoeKernelLauncher.h"
+#include "test/cpp/benchmarks/benchmarkUtils.h"
+#include "test/cpp/utils/utils.h"
+
+namespace {
+constexpr float ERROR_ABS = 1e-5f;
+constexpr int EXPERTS_NUM = 128;
+constexpr int HIDDEN_SIZE = 2048;
+constexpr int EXPERT_INTERMEDIATE_SIZE = 768;
+constexpr int TOPK = 8;
+constexpr int THREADS = 128;
+constexpr int BLOCKS = 304;
+constexpr int REDUCTION_TILE_SIZE = 512;
+constexpr int REDUCTION_CHUNKS_PER_TOKEN = (HIDDEN_SIZE / REDUCTION_TILE_SIZE);
+
+hipError_t run(moe::IMoeKernelLauncher* launcher, const void* tokens,
+               const void* gateWeights, const void* ffn1ExpertWeights,
+               const void* ffn2ExpertWeights, void* output, int tokensNum,
+               hipStream_t stream) {
+  return launcher->Launch(tokens, gateWeights, ffn1ExpertWeights,
+                          ffn2ExpertWeights, output, tokensNum, stream);
+}
+
+void PerformBenchmark(int numTokens) {
+  const int TOKENS_SIZE = numTokens * HIDDEN_SIZE;
+  constexpr int GATE_WEIGHTS_SIZE = HIDDEN_SIZE * EXPERTS_NUM;
+  constexpr int EXPERTS_FNN1_WEIGHTS_SIZE =
+      EXPERTS_NUM * HIDDEN_SIZE * EXPERT_INTERMEDIATE_SIZE * 2;
+  constexpr int EXPERTS_FNN2_WEIGHTS_SIZE =
+      EXPERTS_NUM * HIDDEN_SIZE * EXPERT_INTERMEDIATE_SIZE;
+  const int FINAL_OUTPUT_SIZE = numTokens * HIDDEN_SIZE;
+
+  std::vector<float> tokens_host =
+      RandomVector<float>(TOKENS_SIZE, -1.0f, 1.0f);
+
+  float* tokens_device = nullptr;
+  float* gateWeights_device = nullptr;
+  float* expertFFN1Weights_device = nullptr;
+  float* expertFFN2Weights_device = nullptr;
+  float* finalOutput_device = nullptr;
+
+  hipStream_t stream = 0;
+  moe::IMoeKernelLauncher* launcher = nullptr;
+  HIP_ERROR_ASSERT(CreateLauncher(&launcher, stream, numTokens));
+
+  HIP_ERROR_ASSERT(hipMalloc(&tokens_device, numTokens * sizeof(float)));
+  HIP_ERROR_ASSERT(
+      hipMalloc(&gateWeights_device, GATE_WEIGHTS_SIZE * sizeof(float)));
+  HIP_ERROR_ASSERT(hipMalloc(&expertFFN1Weights_device,
+                             EXPERTS_FNN1_WEIGHTS_SIZE * sizeof(float)));
+  HIP_ERROR_ASSERT(hipMalloc(&expertFFN2Weights_device,
+                             EXPERTS_FNN2_WEIGHTS_SIZE * sizeof(float)));
+  HIP_ERROR_ASSERT(
+      hipMalloc(&finalOutput_device, FINAL_OUTPUT_SIZE * sizeof(float)));
+
+  HIP_ERROR_ASSERT(hipMemcpy(tokens_device, tokens_host.data(),
+                             numTokens * sizeof(float), hipMemcpyHostToDevice));
+
+  const float avg_ms = test::bench::Benchmark(
+      10, 10, run, launcher, tokens_device, gateWeights_device,
+      expertFFN1Weights_device, expertFFN2Weights_device, finalOutput_device,
+      numTokens, stream);
+
+  HIP_ERROR_ASSERT(hipDeviceSynchronize())
+  HIP_ERROR_ASSERT(hipGetLastError());
+
+  // Free device memory.
+  HIP_ERROR_ASSERT(hipFree(tokens_device));
+  HIP_ERROR_ASSERT(hipFree(gateWeights_device));
+  HIP_ERROR_ASSERT(hipFree(expertFFN1Weights_device));
+  HIP_ERROR_ASSERT(hipFree(expertFFN2Weights_device));
+  HIP_ERROR_ASSERT(hipFree(finalOutput_device));
+
+  HIP_ERROR_ASSERT(DestroyLauncher(launcher, stream));
+
+  std::cout << "Moe kernel avg time for num of tokens: " << numTokens << " is "
+            << avg_ms << " ms." << std::endl;
+}
+
+#define DECLARE_BENCHMARK(tokenNum)                    \
+  TEST(Benchmark, MoeBenchmark_TokensNum_##tokenNum) { \
+    PerformBenchmark(tokenNum);                        \
+  }
+
+DECLARE_BENCHMARK(1)
+DECLARE_BENCHMARK(16)
+}  // namespace
