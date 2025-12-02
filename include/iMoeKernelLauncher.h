f2389991d367aa063ada0cbb71ab03bd698c7444
Piotr Kowalczyk <piotr@a2labs.com>
2025-12-02 16:23:00 +0000

    [moe]:[api]: Refactoring, weights are needed to initalize moe.

--- include/iMoeKernelLauncher.h
+++ include/iMoeKernelLauncher.h
@@ -9,10 +9,8 @@ namespace moe {
 class IMoeKernelLauncher {
  public:
   // Launches the kernel.
-  virtual hipError_t Launch(const void* tokens, const void* gateWeights,
-                            const void* ffn1ExpertWeights,
-                            const void* ffn2ExpertWeights, void* output,
-                            int tokensNum, hipStream_t stream) = 0;
+  virtual hipError_t Launch(const void* tokens, void* output, int tokensNum,
+                            hipStream_t stream) = 0;
 
   // Dtor
   virtual ~IMoeKernelLauncher();
@@ -22,7 +20,10 @@ class IMoeKernelLauncher {
 
 // Creates launcher.
 extern "C" hipError_t CreateLauncher(moe::IMoeKernelLauncher** launcher,
-                                     hipStream_t stream, int maxTokens);
+                                     const void* gateWeights,
+                                     const void* ffn1ExpertWeights,
+                                     const void* ffn2ExpertWeights,
+                                     int maxTokens, hipStream_t stream);
 
 // Destroys launcher.
 extern "C" hipError_t DestroyLauncher(moe::IMoeKernelLauncher* launcher,
