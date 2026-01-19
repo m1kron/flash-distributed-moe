6a53a1effd2fa9e72ea9eaffd7f39eb6c1c57aa6
Piotr Kowalczyk <piotr@a2labs.com>
2026-01-19 12:34:59 +0000

    [moe]: Enabled rocshmem test on 4 GPUS

--- scripts/buildAndRun.sh
+++ scripts/buildAndRun.sh
@@ -11,6 +11,8 @@ python3 python/setup.py build_ext
 
 # Run cpp tests
 HIP_VISIBLE_DEVICES=0 ../build/test/singleGPU/cpp/singleGPU_unittests_app
+../build/test/multiGPU/cpp/multiGPU_unittests_app
+
 
 # Run pytest
 HIP_VISIBLE_DEVICES=0 PYTHONPATH=../build pytest
