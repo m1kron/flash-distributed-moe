dfbd7e83b82aa48daecf4f1db14aaf3010232b25
Piotr Kowalczyk <piotr@a2labs.com>
2026-01-27 13:36:26 +0000

    [test][multigpu]: Fixed bug in test setup.

--- test/multiGPU/cpp/moe/moeMultiGPUTests.cpp
+++ test/multiGPU/cpp/moe/moeMultiGPUTests.cpp
@@ -25,45 +25,47 @@ class MoeMultiGPUTests : public MoeDistBaseTest {
         inputCPU.expertFFN2Weights_host.data(), tokensNum, EXPERTS_NUM,
         HIDDEN_SIZE, TOPK, EXPERT_INTERMEDIATE_SIZE);
 
-    ExecuteInSeparateProcesses(1, [&inputCPU, &refOut](
-                                      const moe::DistributedUniqueId& duid,
-                                      int rank, int worldSize) {
-      test::MoeInputGPU gpuAlloc;
-      test::AllocateInputGPU(inputCPU, gpuAlloc, rank, worldSize);
+    ExecuteInSeparateProcesses(
+        worldSize, [&inputCPU, &refOut](const moe::DistributedUniqueId& duid,
+                                        int rank, int worldSize) {
+          test::MoeInputGPU gpuAlloc;
+          test::AllocateInputGPU(inputCPU, gpuAlloc, rank, worldSize);
 
-      std::cout << "Rank " << rank << " will process " << gpuAlloc.tokensNum
-                << " tokens" << std::endl;
+          std::cout << "Rank " << rank << " will process " << gpuAlloc.tokensNum
+                    << " tokens" << std::endl;
 
-      hipStream_t stream = 0;
-      moe::IMoeKernelLauncher* launcher = nullptr;
-      HIP_ERROR_ASSERT(CreateLauncher(
-          &launcher, gpuAlloc.gateWeights_device,
-          gpuAlloc.expertFFN1Weights_device, gpuAlloc.expertFFN2Weights_device,
-          gpuAlloc.tokensNum + 2, stream, duid, rank, worldSize));
+          hipStream_t stream = 0;
+          moe::IMoeKernelLauncher* launcher = nullptr;
+          HIP_ERROR_ASSERT(CreateLauncher(
+              &launcher, gpuAlloc.gateWeights_device,
+              gpuAlloc.expertFFN1Weights_device,
+              gpuAlloc.expertFFN2Weights_device, gpuAlloc.tokensNum + 2, stream,
+              duid, rank, worldSize));
 
-      HIP_ERROR_ASSERT(launcher->Launch(gpuAlloc.tokens_device,
-                                        gpuAlloc.finalOutput_device,
-                                        gpuAlloc.tokensNum, stream));
+          HIP_ERROR_ASSERT(launcher->Launch(gpuAlloc.tokens_device,
+                                            gpuAlloc.finalOutput_device,
+                                            gpuAlloc.tokensNum, stream));
 
-      HIP_ERROR_ASSERT(hipDeviceSynchronize())
-      HIP_ERROR_ASSERT(hipGetLastError());
+          HIP_ERROR_ASSERT(hipDeviceSynchronize())
+          HIP_ERROR_ASSERT(hipGetLastError());
 
-      std::vector<float> output_host(refOut.size(), 0);
+          std::vector<float> output_host(gpuAlloc.tokensNum * HIDDEN_SIZE, 0);
 
-      HIP_ERROR_ASSERT(
-          hipMemcpy(output_host.data(), gpuAlloc.finalOutput_device,
-                    output_host.size() * sizeof(float), hipMemcpyDeviceToHost));
+          HIP_ERROR_ASSERT(hipMemcpy(
+              output_host.data(), gpuAlloc.finalOutput_device,
+              output_host.size() * sizeof(float), hipMemcpyDeviceToHost));
 
-      FreeInputGPU(gpuAlloc);
+          FreeInputGPU(gpuAlloc);
 
-      HIP_ERROR_ASSERT(DestroyLauncher(launcher, stream));
+          HIP_ERROR_ASSERT(DestroyLauncher(launcher, stream));
 
-      const auto refOutThisRank = test::ShardRefOutput(refOut, rank, worldSize);
+          const auto refOutThisRank =
+              test::ShardRefOutput(refOut, rank, worldSize);
 
-      ASSERT_EQ(gpuAlloc.tokensNum, refOutThisRank.size() / HIDDEN_SIZE);
+          ASSERT_EQ(gpuAlloc.tokensNum, refOutThisRank.size() / HIDDEN_SIZE);
 
-      CheckAgainstRefBuffer(output_host, refOutThisRank, ERROR_ABS);
-    });
+          CheckAgainstRefBuffer(output_host, refOutThisRank, ERROR_ABS);
+        });
   }
 };
 
