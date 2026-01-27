750dc5f6d0f209fc224786e97b3fba95af82e639
Piotr Kowalczyk <piotr@a2labs.com>
2026-01-27 11:23:15 +0000

    [test][multigou]: Refactoring, introduced MoeMultiGPUTests.

--- test/multiGPU/cpp/moe/moeDistTest.cpp
+++ test/multiGPU/cpp/moe/moeDistTest.cpp
@@ -14,53 +14,64 @@ constexpr int EXPERT_INTERMEDIATE_SIZE =
     moe::MoeImplMetadata::MOE_PROBLEM_CONFIG::EXPERT_INTERMEDIATE_SIZE;
 constexpr int TOPK = moe::MoeImplMetadata::MOE_PROBLEM_CONFIG::TOPK;
 
-TEST_F(MoeDistBaseTest, BaseTest) {
-  const test::MoeInputCPU inputCPU = test::GenerateMoeInputCPU(TOKENS_NUM);
+class MoeMultiGPUTests : public MoeDistBaseTest {
+ public:
+  void Execute(const test::MoeInputCPU& inputCPU, int worldSize) {
+    const int tokensNum = inputCPU.tokens_host.size() / HIDDEN_SIZE;
+    const auto refOut = test::refFullMoe(
+        inputCPU.tokens_host.data(), inputCPU.gateWeights_host.data(),
+        inputCPU.expertFFN1Weights_host.data(),
+        inputCPU.expertFFN2Weights_host.data(), tokensNum, EXPERTS_NUM,
+        HIDDEN_SIZE, TOPK, EXPERT_INTERMEDIATE_SIZE);
 
-  auto refOut = test::refFullMoe(
-      inputCPU.tokens_host.data(), inputCPU.gateWeights_host.data(),
-      inputCPU.expertFFN1Weights_host.data(),
-      inputCPU.expertFFN2Weights_host.data(), TOKENS_NUM, EXPERTS_NUM,
-      HIDDEN_SIZE, TOPK, EXPERT_INTERMEDIATE_SIZE);
+    ExecuteInSeparateProcesses(1, [&inputCPU, &refOut](
+                                      const moe::DistributedUniqueId& duid,
+                                      int rank, int worldSize) {
+      test::MoeInputGPU gpuAlloc;
+      test::AllocateInputGPU(inputCPU, gpuAlloc, rank, worldSize);
 
-  ExecuteInSeparateProcesses(1, [&inputCPU, &refOut](
-                                    const moe::DistributedUniqueId& duid,
-                                    int rank, int worldSize) {
-    test::MoeInputGPU gpuAlloc;
-    test::AllocateInputGPU(inputCPU, gpuAlloc, rank, worldSize);
+      std::cout << "Rank " << rank << " will process "
+                << gpuAlloc.tokensNum <<  " tokens" << std::endl;
 
-    hipStream_t stream = 0;
-    moe::IMoeKernelLauncher* launcher = nullptr;
-    HIP_ERROR_ASSERT(CreateLauncher(
-        &launcher, gpuAlloc.gateWeights_device,
-        gpuAlloc.expertFFN1Weights_device, gpuAlloc.expertFFN2Weights_device,
-        TOKENS_NUM + 2, stream, duid, rank, worldSize));
+      hipStream_t stream = 0;
+      moe::IMoeKernelLauncher* launcher = nullptr;
+      HIP_ERROR_ASSERT(CreateLauncher(
+          &launcher, gpuAlloc.gateWeights_device,
+          gpuAlloc.expertFFN1Weights_device, gpuAlloc.expertFFN2Weights_device,
+          gpuAlloc.tokensNum + 2, stream, duid, rank, worldSize));
 
-    HIP_ERROR_ASSERT(launcher->Launch(gpuAlloc.tokens_device,
-                                      gpuAlloc.finalOutput_device,
-                                      gpuAlloc.tokensNum, stream));
+      HIP_ERROR_ASSERT(launcher->Launch(gpuAlloc.tokens_device,
+                                        gpuAlloc.finalOutput_device,
+                                        gpuAlloc.tokensNum, stream));
 
-    HIP_ERROR_ASSERT(hipDeviceSynchronize())
-    HIP_ERROR_ASSERT(hipGetLastError());
+      HIP_ERROR_ASSERT(hipDeviceSynchronize())
+      HIP_ERROR_ASSERT(hipGetLastError());
 
-    std::vector<float> output_host(refOut.size(), 0);
+      std::vector<float> output_host(refOut.size(), 0);
 
-    HIP_ERROR_ASSERT(hipMemcpy(output_host.data(), gpuAlloc.finalOutput_device,
-                               output_host.size() * sizeof(float),
-                               hipMemcpyDeviceToHost));
+      HIP_ERROR_ASSERT(
+          hipMemcpy(output_host.data(), gpuAlloc.finalOutput_device,
+                    output_host.size() * sizeof(float), hipMemcpyDeviceToHost));
 
-    // Free device memory.
-    FreeInputGPU(gpuAlloc);
+      // Free device memory.
+      FreeInputGPU(gpuAlloc);
 
-    HIP_ERROR_ASSERT(DestroyLauncher(launcher, stream));
+      HIP_ERROR_ASSERT(DestroyLauncher(launcher, stream));
 
-    const auto refOutThisRank = test::ShardRefOutput(refOut, rank, worldSize);
+      const auto refOutThisRank = test::ShardRefOutput(refOut, rank, worldSize);
 
-    ASSERT_EQ(gpuAlloc.tokensNum, refOutThisRank.size() / HIDDEN_SIZE);
+      ASSERT_EQ(gpuAlloc.tokensNum, refOutThisRank.size() / HIDDEN_SIZE);
 
-    std::cout << "Out tokens for rank " << rank << " is "
-              << refOutThisRank.size() / HIDDEN_SIZE << std::endl;
-    // CheckAgainstRefBuffer(output_host, refOutThisRank, ERROR_ABS);
-  });
+      CheckAgainstRefBuffer(output_host, refOutThisRank, ERROR_ABS);
+    });
+  }
+};
+
+TEST_F(MoeMultiGPUTests, worldSize1_tokens1) {
+  const int tokensNum = 1;
+  const test::MoeInputCPU inputCPU = test::GenerateMoeInputCPU(tokensNum);
+
+  Execute(inputCPU, 1);
 }
+
 }  // namespace
