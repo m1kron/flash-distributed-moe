32cdc684957c940932b0512dce1943f70b3e9955
Piotr Kowalczyk <piotr@a2labs.com>
2026-01-28 10:50:18 +0000

    [moe][remote]: Refactoring of RemoteComManager.

--- src/hip/kernel/remote/remoteComManager.h
+++ src/hip/kernel/remote/remoteComManager.h
@@ -1,6 +1,6 @@
 #pragma once
-#include "src/hip/utils/hipDeviceUtils.h"
 #include "src/hip/common/rocshmem/rocshmem.h"
+#include "src/hip/utils/hipDeviceUtils.h"
 
 namespace moe {
 
@@ -10,6 +10,7 @@ class RemoteComManager {
                            int worldSize);
   __host__ hipError_t Deinit();
   __device__ int GetWorldSize() const;
+  __device__ int GetRank() const;
 
  private:
   __host__ hipError_t InitRocshmem(const moe::DistributedUniqueId& uid,
@@ -17,6 +18,7 @@ class RemoteComManager {
 
   bool m_isActive = false;
   int m_worldSize = 1;
+  int m_rank = 0;
 };
 
 __constant__ RemoteComManager globalRemoteComManager;
@@ -31,8 +33,10 @@ __constant__ RemoteComManager globalRemoteComManager;
 inline __host__ hipError_t RemoteComManager::Init(
     const moe::DistributedUniqueId& uid, int rank, int worldSize) {
   MOE_ASSERT(worldSize > 0);
+  MOE_ASSERT(rank < worldSize);
   m_isActive = worldSize > 1;
   m_worldSize = worldSize;
+  m_rank = rank;
 
   if (m_isActive) {
     HIP_ERROR_CHECK(InitRocshmem(uid, rank, worldSize));
@@ -50,24 +54,23 @@ inline __host__ hipError_t RemoteComManager::Deinit() {
 ////////////////////////////////////////////////////////////////////
 inline __host__ hipError_t RemoteComManager::InitRocshmem(
     const moe::DistributedUniqueId& uid, int rank, int worldSize) {
-  rocshmem::rocshmem_uniqueid_t roc_uid;
+  rocshmem_uniqueid_t roc_uid;
   memcpy(roc_uid.data(), uid.data, sizeof(uid.data));
 
-  rocshmem::rocshmem_init_attr_t attr;
+  rocshmem_init_attr_t attr;
 
-  if (rocshmem::rocshmem_set_attr_uniqueid_args(rank, worldSize, &roc_uid,
-                                                &attr) != 0) {
+  if (rocshmem_set_attr_uniqueid_args(rank, worldSize, &roc_uid, &attr) != 0) {
     MOE_ERROR_LOG("rocshmem_set_attr_uniqueid_args failed");
     return hipErrorUnknown;
   }
-  if (rocshmem::rocshmem_init_attr(rocshmem::ROCSHMEM_INIT_WITH_UNIQUEID,
-                                   &attr) != 0) {
+
+  if (rocshmem_init_attr(ROCSHMEM_INIT_WITH_UNIQUEID, &attr) != 0) {
     MOE_ERROR_LOG("rocshmem_init_attr failed");
     return hipErrorUnknown;
   }
 
-  const int rocshmem_rank = rocshmem::rocshmem_my_pe();
-  const int rocshmem_size = rocshmem::rocshmem_n_pes();
+  const int rocshmem_rank = rocshmem_my_pe();
+  const int rocshmem_size = rocshmem_n_pes();
 
   MOE_ASSERT(rocshmem_rank == rank);
   MOE_ASSERT(rocshmem_size == worldSize);
@@ -87,5 +90,8 @@ inline __host__ hipError_t RemoteComManager::InitRocshmem(
 inline __device__ int RemoteComManager::GetWorldSize() const {
   return m_worldSize;
 }
+
+//////////////////////////////////////////////////////////////////
+inline __device__ int RemoteComManager::GetRank() const { return m_rank; }
 
 }  // namespace moe
